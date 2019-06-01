b20a7432f2ddeddf44788eb13b04e0731cf1354b
Arthamios <adrien.fabre@epitech.eu>
2019-06-02 01:51:01 +0200

    Implemented bitcoin

--- src/IchimokuStrategy.js
+++ src/IchimokuStrategy.js
@@ -10,22 +10,28 @@ const Order = require('./Order');
 
 module.exports = class IchimokuStrat {
     constructor() {
-        this.tenkanVSkijun = {};
-        this.timeSinceCrossUp = [];
-        this.timeSinceCrossDown = [];
+        this.tenkanVSkijun = new Map();
+        this.tenkanVSkijun.set('USDT_ETH', undefined);
+        this.tenkanVSkijun.set('USDT_BTC', undefined);
+        this.timeSinceCrossUp = new Map();
+        this.timeSinceCrossUp.set('USDT_ETH', []);
+        this.timeSinceCrossUp.set('USDT_BTC', []);
+        this.timeSinceCrossDown = new Map();
+        this.timeSinceCrossDown.set('USDT_ETH', []);
+        this.timeSinceCrossDown.set('USDT_BTC', []);
     }
 
     execute(gameSettings, state) {
         const dollars = state.stacks['USDT'];
         const ETHClosePrice = state.charts['USDT_ETH'].getLastCandle().close;
         const BTCClosePrice = state.charts['USDT_BTC'].getLastCandle().close;
-        const ETHAmount = dollars / ( ETHClosePrice);
+        const ETHAmount = dollars / (2 * ETHClosePrice);
         const BTCAmount = dollars / (2 * BTCClosePrice);
 
         const ETHOrder = this.findOrder(state.charts, 'USDT_ETH', ETHAmount, state.stacks["ETH"]);
-        //const BTCOrder = this.findOrder(state.charts,'USDT_BTC', BTCAmount);
+        const BTCOrder = this.findOrder(state.charts,'USDT_BTC', BTCAmount, state.stacks["BTC"]);
 
-        return IchimokuStrat.pass_order(ETHOrder, null).toString();
+        return IchimokuStrat.pass_order(ETHOrder, BTCOrder).toString();
     }
     
     static pass_order(ETHOrder, BTCOrder) {
@@ -84,9 +90,9 @@ module.exports = class IchimokuStrat {
 
         this.updateCrossing(TenkanSen, KijunSen, pair);
         this.updateTimeSinceCross(pair);
-        if (amount > 0 && this.timeSinceCrossUp.length > 0 && ClosePrice > SenkouSpanA && ClosePrice > SenkouSpanB)
+        if (amount > 0 && this.timeSinceCrossUp.get(pair).length > 0 && ClosePrice > SenkouSpanA && ClosePrice > SenkouSpanB)
             return new Order('buy', pair, amount);
-        if (CurrencyPossessed > 0 && this.timeSinceCrossDown.length > 0 && ClosePrice < SenkouSpanB && ClosePrice < SenkouSpanA)
+        if (CurrencyPossessed > 0 && this.timeSinceCrossDown.get(pair).length > 0 && ClosePrice < SenkouSpanB && ClosePrice < SenkouSpanA)
             return new Order('sell', pair, CurrencyPossessed);
         return null;
     }
@@ -94,27 +100,27 @@ module.exports = class IchimokuStrat {
     updateCrossing(TenkanSen, KijunSen, pair) {
         const crossing = TenkanSen > KijunSen;
 
-        if (crossing !== this.tenkanVSkijun[pair]) {
-            this.tenkanVSkijun[pair] = crossing;
+        if (crossing !== this.tenkanVSkijun.get(pair)) {
+            this.tenkanVSkijun.set(pair, crossing);
             if (crossing)
-                this.timeSinceCrossUp.push(0);
+                this.timeSinceCrossUp.get(pair).push(0);
             else
-                this.timeSinceCrossDown.push(0);
+                this.timeSinceCrossDown.get(pair).push(0);
         }
     }
 
     updateTimeSinceCross(pair) {
-        this.timeSinceCrossUp.forEach(function (val, idx) {
+        this.timeSinceCrossUp.get(pair).forEach(function (val, idx) {
             ++(this[idx]);
-        }, this.timeSinceCrossUp);
-        this.timeSinceCrossDown.forEach(function (val, idx) {
+        }, this.timeSinceCrossUp.get(pair));
+        this.timeSinceCrossDown.get(pair).forEach(function (val, idx) {
             ++(this[idx]);
-        }, this.timeSinceCrossDown);
-        this.timeSinceCrossUp = this.timeSinceCrossUp.filter(function (val) {
+        }, this.timeSinceCrossDown.get(pair));
+        this.timeSinceCrossUp.set(pair, this.timeSinceCrossUp.get(pair).filter(function (val) {
             return val <= 4
-        });
-        this.timeSinceCrossDown = this.timeSinceCrossDown.filter(function (val) {
+        }));
+        this.timeSinceCrossDown.set(pair, this.timeSinceCrossDown.get(pair).filter(function (val) {
             return val <= 4
-        });
+        }));
     }
 };
